f6abaa4935b811b5996a19bda1e9dd2b6e2b06a8
Abhishek-Kartik <abhishek123cu@gmail.com>
2026-01-18 14:49:33 +0530

    Code fix

--- Level2/src/main/java/com/example/level2/entity/JournalEntity.java
+++ Level2/src/main/java/com/example/level2/entity/JournalEntity.java
@@ -3,7 +3,6 @@ package com.example.level2.entity;
 
 import lombok.*;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
