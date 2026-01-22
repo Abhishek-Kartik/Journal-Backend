cea5392d0f91666dd47bce638839d8ad56b2ba5a
Abhishek-Kartik <abhishek123cu@gmail.com>
2026-01-22 11:34:33 +0530

    build fix

--- Level2/src/test/java/com/example/level2/JournalApplicationTests.java
+++ Level2/src/test/java/com/example/level2/JournalApplicationTests.java
@@ -2,10 +2,16 @@ package com.example.level2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class JournalApplicationTests {
-
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl(); // empty stub
+    }
 	@Test
 	void contextLoads() {
 	}
